c2d405161429e7a1c84fcbce2c16dbbd7f659c61
Farhan Rahman <farhanrahman@Fuads-MacBook-Air.local>
2017-06-19 12:07:53 -0400

    Added some shit

--- main/script.py
+++ main/script.py
@@ -21,6 +21,38 @@ IN ONE COMMAND, this script should....
 ## Repo urls to clone.
 repo_urls = ['https://github.com/bubblegroup/bubble', 'https://github.com/jphaas/bubble_private']
 
+## Questions to confirm
+confirmed_1 = True 	# 'Did you open bubble-app.slack.com?'
+confirmed_2 = True	# 'Please go to bubble-bot and type in "new_developer"'
+confirmed_3 = True	# 'Did you open bubble-app.slack.com?'
+
+# Function that sends confirmation questions.
+def confirm_question(question):
+	question = raw_input(question + '(y / n)')
+
+	if (question == 'y' or question == 'yes' or question == 'Y'):
+		return True
+	elif (question == 'n' or question == 'N' or question == 'no'):
+		return False
+	else:
+		return False
+
+# Function that creates files for us in other paths.
+def file_creator(path, data, filename):
+
+	print('Creating a file named ' +  '"' + filename + '"' + ' inside of ' + path)
+
+	# Create a file with the open function.
+	f = open(path + filename ,"w+")
+
+	# Write the new data inside of the file.
+	f.write(data)
+
+	# Print closing message.
+	print('Created ' + '"' + filename + '"'   + '')
+
+	# Close the fie stream.
+	f.close()
 
 # Function that downloads the repos.
 def download_repo(repo):
@@ -57,6 +89,8 @@ def download_repo(repo):
 			print('ERORR - Error while cloning ' + repo)
 
 
+def file_editor(path, old_tag, new_tag):
+	print('FILE EDITED')
 
 ################################################################################
 '''
@@ -125,5 +159,17 @@ while confirmed_3 == False:
 		# Path for the credentials file.
 		credential_path = os.getcwd() + '/bubble/lib/'
 
+		# Path for the file that we should edit.
+		edit_path = 'HAHAHA'
+
+		# Old tag to locate the part of the file that we want to edit.
+		old_tag = 'AHAHAH'
+
+		# New data to place inside of the old tag.
+		new_tag = 'ASDFSADF'
+
 		# Pass data to file creator, and create the file with the data.
 		file_creator(credential_path, input_credentials, 'credentials.coffee')
+
+		# Edit the file, so the localhost becomes local.bubble.is
+		file_editor(edit_path, old_tag, new_tag)
